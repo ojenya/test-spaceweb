5c07a1e5893e7fb72703a7b84909b01be0724930
Jenya <jeenjenya@gmail.com>
2020-09-14 00:57:08 +0300

    Updates

---
+++ precache-manifest.fac58698ff841fa00e86f609f2ec1ef9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf3d38b7253c4814845064441f213eb",
+    "url": "/test-spaceweb/index.html"
+  },
+  {
+    "revision": "857a7ce8caa8ac9fc695",
+    "url": "/test-spaceweb/static/css/main.20ca1608.chunk.css"
+  },
+  {
+    "revision": "2f13a73bfe2e6bc032f8",
+    "url": "/test-spaceweb/static/js/2.67ceeb4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-spaceweb/static/js/2.67ceeb4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857a7ce8caa8ac9fc695",
+    "url": "/test-spaceweb/static/js/main.cd86c848.chunk.js"
+  },
+  {
+    "revision": "f0b673d6bf048342b881",
+    "url": "/test-spaceweb/static/js/runtime-main.b7ee8182.js"
+  },
+  {
+    "revision": "59a5bfcb09a0d460c467a4773f9f5d57",
+    "url": "/test-spaceweb/static/media/logo.59a5bfcb.svg"
+  }
+]);
